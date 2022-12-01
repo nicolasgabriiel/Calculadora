af137686050dc78b1ee4591ec8ab1cd7a75531a7
Nicolas <nicolasgabriel.developer@gmail.com>
2022-11-30 21:53:22 -0300

    Melhorando os comentários do código

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+// Função que insere os numeros no html, o parametro num está dentro da função onclick no html
 function insert(num)
 {
     var numero = document.getElementById('resultado').innerHTML;
@@ -7,38 +7,40 @@ function insert(num)
     var resultado = document.getElementById('resultado').innerHTML;
     console.log(resultado.length);
 
+//Esse if serve para a conta não passar de determinado número de caracteres
     if(resultado.length == 15){
         alert('Essa calculadora não aceita mais números do que isso, tente calculos mais simples. Por favor não tente prosseguir inserindo mais números')
     }else if(resultado.length > 15){
         clean()
     }
 }
+//Essa função serve para limpar o html
 function clean()
 {
     document.getElementById('resultado').innerHTML = "";
 }
+//Essa função serve para apagar o ultimo digito exibido
 function back()
 {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+//Essa função verifica se existe numeros sendo exibido, transforma a string, faz o calculo e exibe o resultado
 function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado != "")
-    {
+    if(resultado != ""){
         
         document.getElementById('resultado').innerHTML = eval(resultado);
       
-    }
-    else
-    {
+    }else{
+      
         document.getElementById('resultado').innerHTML = ""
+  
     }
     console.log(resultado.length)
-
 }
-var resultado = document.getElementById('resultado').innerHTML;
+
 
 
 
