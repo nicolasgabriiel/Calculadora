0670968a6cddf87b565c066453c39a01d0287730
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-01 23:01:59 -0300

    Calculadora funcionando com teclado

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ function insert(num)
 
     var resultado = document.getElementById('resultado').innerHTML;
     console.log(resultado.length);
+    verificarTamanho()
 
 //Esse if serve para a conta não passar de determinado número de caracteres
+   
+function verificarTamanho () {
     if(resultado.length == 15){
         alert('Essa calculadora não aceita mais números do que isso, tente calculos mais simples. Por favor não tente prosseguir inserindo mais números')
     }else if(resultado.length > 15){
         clean()
     }
+}
 }
 //Essa função serve para limpar o html
 function clean()
@@ -41,19 +45,69 @@ function calcular()
     console.log(resultado.length)
 }
 
-//Armazenando os botoes em lista
+  //Funcionamento por teclado
+  document.body.onkeydown = function(e) {
+  
+    function verificarTamanho () {
+        if(resultado.length == 15){
+            alert('Essa calculadora não aceita mais números do que isso, tente calculos mais simples. Por favor não tente prosseguir inserindo mais números')
+        }else if(resultado.length > 15){
+            clean()
+        }
+    }
+  
+    for(let i= 0 ; i < 10 ; i++){
 
+    if (e.key == i){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + i ;
+        var resultado = document.getElementById('resultado').innerHTML;
+        verificarTamanho()
+    }}
 
-const teclas = document.querySelectorAll('.tecla')
+    if (e.key == "/"){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + '/';
+        var resultado = document.getElementById('resultado').innerHTML;
+        verificarTamanho()
+    }
+    
+    if (e.key == "*" || e.key == 'x'){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + '*';
+        var resultado = document.getElementById('resultado').innerHTML;
+        verificarTamanho()
+    }
 
-console.log(teclas)
+    if (e.key == "-"){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + '-';
+        var resultado = document.getElementById('resultado').innerHTML;
+        verificarTamanho()
+    }
 
+    if (e.key == "+"){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + '+';
+        var resultado = document.getElementById('resultado').innerHTML;
+        verificarTamanho()
+    }
 
+    if (e.key == "Delete"){ 
+        clean()
+    }
 
+    if (e.key == "Backspace"){ 
+       back()
+    }
 
-
-
-
+    if (e.key == "="){ 
+        calcular()
+        verificarTamanho()
+    }
+    
+    
+  }
 
 
 
