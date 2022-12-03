2fd8e84ccfaa0f65b94b868ea610eaf2eb2b5d56
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-02 22:10:38 -0300

    Fazendo toda a estrutura para funcionamento de teclado

--- js/main.js
+++ js/main.js
@@ -45,9 +45,26 @@ function calcular()
     console.log(resultado.length)
 }
 
+ let tecla;
+
+ const dividir = document.getElementById('dividir')
+ const mais = document.getElementById('mais')
+ const menos = document.getElementById('menos')
+ const vezes = document.getElementById('vezes')
+ const ponto = document.getElementById('ponto')
+ const igual = document.getElementById('calcular')
+ const del = document.getElementById('del')
+ const reset = document.getElementById('reset')
+
+
+
+
   //Funcionamento por teclado
   document.body.onkeydown = function(e) {
   
+    var resultado = document.getElementById('resultado').innerHTML;
+
+
     function verificarTamanho () {
         if(resultado.length == 15){
             alert('Essa calculadora não aceita mais números do que isso, tente calculos mais simples. Por favor não tente prosseguir inserindo mais números')
@@ -57,58 +74,103 @@ function calcular()
     }
   
     for(let i= 0 ; i < 10 ; i++){
-
+        
     if (e.key == i){ 
         var numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = numero + i ;
-        var resultado = document.getElementById('resultado').innerHTML;
+        
+        tecla = document.getElementById('tecla' +i)
+        
+        tecla.classList.add("active")
+
         verificarTamanho()
     }}
 
     if (e.key == "/"){ 
         var numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = numero + '/';
-        var resultado = document.getElementById('resultado').innerHTML;
+    
+        dividir.classList.add('active')
+
         verificarTamanho()
     }
     
     if (e.key == "*" || e.key == 'x'){ 
         var numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = numero + '*';
-        var resultado = document.getElementById('resultado').innerHTML;
+    
+        vezes.classList.add('active')
+        
         verificarTamanho()
     }
 
     if (e.key == "-"){ 
         var numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = numero + '-';
-        var resultado = document.getElementById('resultado').innerHTML;
+       
+                
+        menos.classList.add('active')
+
         verificarTamanho()
     }
 
     if (e.key == "+"){ 
         var numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = numero + '+';
-        var resultado = document.getElementById('resultado').innerHTML;
+
+        mais.classList.add('active')
+        
+        verificarTamanho()
+    }
+
+    if (e.key == "." || e.key == ","){ 
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + '.';
+       
+        ponto.classList.add('active')
         verificarTamanho()
     }
 
     if (e.key == "Delete"){ 
+        reset.classList.add('special')
         clean()
     }
 
     if (e.key == "Backspace"){ 
-       back()
+        del.classList.add('special')
+        back()
     }
 
     if (e.key == "="){ 
+        igual.classList.add('unico')
         calcular()
         verificarTamanho()
     }
-    
-    
+    setTimeout(removerClasse, 250);
   }
 
+  function removerClasse(){
+    for(let i = 0; i < 10; i++){
+
+    const teclas = document.getElementById('tecla' + i)
+    
+    teclas.classList.remove('active') 
+
+}
+    dividir.classList.remove('active') 
+    vezes.classList.remove('active') 
+    mais.classList.remove('active') 
+    menos.classList.remove('active') 
+    ponto.classList.remove('active') 
+    del.classList.remove('special')
+    reset.classList.remove('special')
+    igual.classList.remove('unico')
+
+}
+
+  
+    
+   
 
 
 
